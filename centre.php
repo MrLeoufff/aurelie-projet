5102b2d9b20095d0971eae75fccfb7f940fbc132
mrleoufff <mrleoufff@gmail.com>
2025-01-06 13:13:06 +0100

    Update centre style

--- centre.php
+++ centre.php
@@ -1,25 +1,9 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="styles.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
+<?php include 'includes/header.php';?>
 
 
-
-    <title>Document</title>
-</head>
-<body>
-    
-  
-<div class="container">   
+<div class="container mt-3">
     <div class="topnav">
-    <a href=Accueil.php aria-label="Accueil">Accueil</a>
+        <a href=index.php aria-label="Accueil">Accueil</a>
         <a href="quiSommesNous.php" aria-label="Qui sommes-nous ?">Qui sommes-nous?</a>
         <a href="carte.php" aria-label="Nous contacter">Contact</a>
         <a href="lien.php" aria-label="Créer un rendez-vous">Créer un rendez-vous</a>
@@ -27,20 +11,14 @@
         <a class="active" href="centre.php"Voir la liste des centres disponibles">Liste des centres</a>
     </div>
 
-      
-<h3> Exemple de centres medicaux</h3>
-      <div class="container">
-        <img src="assets/labo.jpg "width="900px"heignt='200px'>
-        <img src="assets/bordeaux.jpg "width="400px"heignt='200px'>
-        <img src="assets/cliniqueduparc.jpg "width="400px"heignt='200px'>
-        <img src="assets/hopitalcentral.jpg "width="400px"heignt='200px'>
-        <img src="assets/lille.jpg "width="400px"heignt='200px'>
-        <img src="assets/roubaix.jpg "width="400px"heignt='200px'>
+    <h3> Exemple de centres medicaux</h3>
 
-   
+        <img src="assets/images/labo.jpg "width="900px"heignt='200px'>
+        <img src="assets/images/bordeaux.jpg "width="400px"heignt='200px'>
+        <img src="assets/images/cliniqueduparc.jpg "width="400px"heignt='200px'>
+        <img src="assets/images/hopitalcentral.jpg "width="400px"heignt='200px'>
+        <img src="assets/images/lille.jpg "width="400px"heignt='200px'>
+        <img src="assets/images/roubaix.jpg "width="400px"heignt='200px'>
 
-      </div>
-        
-    </div>
-</body>
-</html>
+    <?php include 'includes/footer.php';?>
+</div>
